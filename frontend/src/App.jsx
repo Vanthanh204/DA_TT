32ef679161df75840a86fe18769b2897ab748d3f
Vanthanh204 <vanthanh20042021@gmail.com>
2026-04-30 15:27:43 +0700

    sua trang admin 3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
